7fb68c7f43bdaa4ef9105dccc72b8f6bfb093723
Nurali_NM3 <nurdtinovnurali@gmail.com>
2023-03-28 11:43:35 +0600

    change converter valute

--- script.js
+++ script.js
@@ -5,26 +5,53 @@ const input = document.querySelector('.input')
 const btn2 = document.querySelector('.btn2')
 const inputBtn = document.querySelector('.input-btn')
 const inputBtn2 = document.querySelector('.input-btn2')
-const title = document.querySelector('.title')
-const title2 = document.querySelector('.title2')
-const title3 = document.querySelector('.title3')
+const eur = document.querySelector('.eur')
+const rub = document.querySelector('.rub')
+const kgs = document.querySelector('.kgs')
+const usd = document.querySelector('.usd')
 const converter =document.querySelector('.converter')
 const submit = document.querySelector('.submit')
+const valut =document.querySelector('#valut')
 
-
+console.log(valut.value)
+//https://api.exchangerate.host/latest?base=kgs&symbols=USD,EUR
 submit.addEventListener('click',() => {
-    let f = converter.value
-    fetch(`https://api.exchangerate.host/latest?base=KGS&symbols=USD,EUR,RUB,&amount=${f}`)
+
+        let money =valut.value
+    let f = converter.value || 0
+    fetch(`https://api.exchangerate.host/latest?base=${money}&symbols=USD,EUR,RUB,KGS,&amount=${f}`)
         .then(response => response.json())
         .then(data =>{
             console.log(data.rates)
-
-
-
-            title.innerHTML = Object.values(data.rates)[0].toFixed(4)
-            title2.innerHTML = Object.values(data.rates)[1].toFixed(4)
-            title3.innerHTML = Object.values(data.rates)[2].toFixed(4)
+            console.log(data)
+            if(money === 'USD'){
+                eur.innerHTML = Object.values(data.rates)[0].toFixed(2)
+                rub.innerHTML = Object.values(data.rates)[2].toFixed(2)
+                kgs.innerHTML = Object.values(data.rates)[1].toFixed(2)
+                usd.innerHTML = ''
+            }else if(money === 'KGS'){
+                eur.innerHTML = Object.values(data.rates)[0].toFixed(2)
+                rub.innerHTML = Object.values(data.rates)[2].toFixed(2)
+                usd.innerHTML = Object.values(data.rates)[3].toFixed(2)
+                kgs.innerHTML = ''
+            }else if(money === 'EUR'){
+                rub.innerHTML = Object.values(data.rates)[2].toFixed(2)
+                kgs.innerHTML = Object.values(data.rates)[1].toFixed(2)
+                usd.innerHTML = Object.values(data.rates)[3].toFixed(2)
+                eur.innerHTML = ''
+            }else if(money === 'RUB'){
+                kgs.innerHTML = Object.values(data.rates)[1].toFixed(2)
+                usd.innerHTML = Object.values(data.rates)[3].toFixed(2)
+                eur.innerHTML = Object.values(data.rates)[0].toFixed(2)
+                rub.innerHTML =  ''
+            }
+            // eur.innerHTML = Object.values(data.rates)[0].toFixed(2)
+            // rub.innerHTML = Object.values(data.rates)[2].toFixed(2)
+            // kgs.innerHTML = Object.values(data.rates)[1].toFixed(2)
+            // usd.innerHTML = Object.values(data.rates)[3].toFixed(2)
         })
+
+
 })
 
 
@@ -60,7 +87,7 @@ btn.addEventListener('click',()=>{
                                <div class="wrap-img">
                                     <img src="${country.flags.png}" alt="">
                                 </div>
-                                    <h3>${country.name.common}</h3>
+                                    <h3>${country.name.common.translations.rus}</h3>
                                     <div class="info">                          
                                      Capital:<h4>${country.capital[0]}</h4>                                 
                                     </div>
